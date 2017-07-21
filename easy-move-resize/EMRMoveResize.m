999d4025e0a37cec8cde4536047293e7353e804a
Daniel Marcotte <dmarcotte@gmail.com>
2017-07-21 15:35:31 -0700

    Clean up overridden synthetic properties

--- easy-move-resize/EMRMoveResize.m
+++ easy-move-resize/EMRMoveResize.m
@@ -2,9 +2,7 @@
 
 @implementation EMRMoveResize
 @synthesize eventTap = _eventTap;
-@synthesize runLoopSource = _runLoopSource;
 @synthesize resizeSection = _resizeSection;
-@synthesize window = _window;
 @synthesize tracking = _tracking;
 @synthesize wndPosition = _wndPosition;
 @synthesize wndSize = _wndSize;
